63a5252689ed822cd796f2e154f944f807d10563
waysaku <waysaku@gmail.com>
2019-07-20 22:36:37 +0900

    fix

--- setups/setup-mac.sh
+++ setups/setup-mac.sh
@@ -2,8 +2,10 @@ echo "start setup for MacOS"
 
 echo "##### Install basic packages"
 brew update > /dev/null 2>&1
-brew install git vim curl tmux > /dev/null 2>&1
+brew install git vim curl tmux reattach-to-user-namespace ricty > /dev/null 2>&1
+brew tap sanemat/font > /dev/null 2>&1
+
+cp -f /usr/local/opt/ricty/share/fonts/Ricty*.ttf ~/Library/Fonts/ && fc-cache -vf > /dev/null 2>&1
 
 source setups/setup-common.sh
-
 echo "export LC_ALL=en_US.UTF-8" >> ~/.bashrc
