07c54c99d4a447881d8deabfdde5b8523370117a
waysaku <waysaku@gmail.com>
2019-07-20 09:20:19 +0000

    tmp

---
+++ setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source setups/setup-ubuntu.sh
+
---
+++ setups/setup-ubuntu.sh
@@ -0,0 +1,28 @@
+echo "start setup for ubuntu"
+
+apt update
+apt install -y git vim curl tmux
+
+mkdir ~/.vim
+cp dotfiles/.gitconfig    ~/
+cp dotfiles/.tmux.conf    ~/
+cp dotfiles/.vimrc        ~/
+cp dotfiles/.bashrc       ~/
+cp dotfiles/.bash_profile ~/
+cp dotfiles/.editorconfig ~/
+
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+sh ./installer.sh ~/.cache/dein
+
+git clone https://github.com/waysaku/vim /tmp/vim
+mkdir ~/.vim/colors
+cp /tmp/vim/colors/dracula.vim ~/.vim/colors/
+
+cp ./git-bashfiles/git-completion.bash ~/
+cp ./git-bashfiles/git-prompt.sh       ~/
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+source ~/.bashrc
+nvm install v10.15.0
+nvm use     v10.15.0
+
