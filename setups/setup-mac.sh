8fef122eeebf63d650382ff3753a833242cf9459
waysaku <waysaku@gmail.com>
2019-08-14 00:48:57 +0900

    add hub command

--- setups/setup-mac.sh
+++ setups/setup-mac.sh
@@ -2,7 +2,7 @@ echo "start setup for MacOS"
 
 echo "##### Install basic packages"
 brew update > /dev/null 2>&1
-brew install git vim curl tmux reattach-to-user-namespace ricty > /dev/null 2>&1
+brew install git vim curl tmux reattach-to-user-namespace ricty hub > /dev/null 2>&1
 brew tap sanemat/font > /dev/null 2>&1
 
 cp -f /usr/local/opt/ricty/share/fonts/Ricty*.ttf ~/Library/Fonts/ && fc-cache -vf > /dev/null 2>&1
