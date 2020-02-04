fcdf0c7195935ee975e0f4fe01ad6eefd5784497
waysaku <waysaku@gmail.com>
2020-02-04 16:10:36 +0900

    add supporting Debian

--- setup.sh
+++ setup.sh
@@ -2,8 +2,12 @@
 if [ "$(uname)" == 'Darwin' ]; then
   source setups/setup-mac.sh
 elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
-  if [ -e /etc/lsb-release ]; then
+  ISSUE=`expr substr "$(head -n 1 /etc/issue)" 1 6`
+  if [ -e /etc/lsb-release ] || [ $ISSUE == 'Debian' ]; then
     source setups/setup-ubuntu.sh
+  else
+    echo "Your Linux distribution is not supported."
+    exit 1
   fi
 else
   echo "Your platform ($(uname -a)) is not supported."
